7959f40f24338bd3f25fe4b9ccd9a9e33f953f0c
Atul Mishra <atulmishrafbd@gmail.com>
2022-11-16 23:20:02 +0530

    Delete Namaste React/src/Day1 directory

--- Namaste React/src/Day1/src.js
+++
@@ -1,7 +0,0 @@
-// Testing through Vanilla JS
-const h1Tag = document.createElement("h1");
-h1Tag.innerText = "Hello World";
-document.querySelector("#jsRoot").appendChild(h1Tag);
-// Testing Through React
-const reactEle = React.createElement("h1", { className: "class1", id: "test1" }, React.createElement("p", null, "Hello World"));
-ReactDOM.render(reactEle, document.querySelector("#root"));
